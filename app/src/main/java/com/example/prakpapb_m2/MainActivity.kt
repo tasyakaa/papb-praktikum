3856b640922156b3ca9320fe84e46bb8766b3648
tasyakaa <tasya.krs@gmail.com>
2024-09-16 22:30:48 +0700

    MainActivity.kt

--- app/src/main/java/com/example/prakpapb_m2/MainActivity.kt
+++ app/src/main/java/com/example/prakpapb_m2/MainActivity.kt
@@ -5,11 +5,20 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountBox
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
@@ -21,9 +30,15 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.buildAnnotatedString
 import androidx.compose.ui.text.font.FontWeight.Companion.Bold
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.withStyle
+import androidx.compose.ui.text.SpanStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import com.example.prakpapb_m2.ui.theme.BlueGrey40
 import com.example.prakpapb_m2.ui.theme.PRAKPAPB_M2Theme
 
 class MainActivity : ComponentActivity() {
@@ -44,8 +59,10 @@ class MainActivity : ComponentActivity() {
 
 @Composable
 fun MyScreen() {
-    var text by remember { mutableStateOf("") }
-    var inputText by remember { mutableStateOf("") }
+    var name by remember { mutableStateOf("") }
+    var inputName by remember { mutableStateOf("") }
+    var nim by remember { mutableStateOf("") }
+    var inputNim by remember { mutableStateOf("") }
 
     Column(
         modifier = Modifier
@@ -55,25 +72,87 @@ fun MyScreen() {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
         Text(
-            text = "Name",
-            style = MaterialTheme.typography.titleLarge.copy(fontWeight = Bold),
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = Bold)) {
+                    append("Name: ")
+                }
+                append(name)
+            },
+            style = MaterialTheme.typography.bodyLarge,
             modifier = Modifier
                 .align(Alignment.Start)
-                .padding(horizontal = 24.dp)
+                .padding(horizontal = 48.dp)
         )
+
+        Spacer(modifier = Modifier.height(4.dp))
+
         Text(
-            text = text,
+            text = buildAnnotatedString {
+                withStyle(style = SpanStyle(fontWeight = Bold)) {
+                    append("NIM: ")
+                }
+                append(nim)
+            },
             style = MaterialTheme.typography.bodyLarge,
-            modifier = Modifier.padding(bottom = 16.dp)
+            modifier = Modifier
+                .align(Alignment.Start)
+                .padding(horizontal = 48.dp)
         )
-        OutlinedTextField(
-            value = inputText,
-            onValueChange = { inputText = it },
-            label = { Text("Enter your name") },
-            modifier = Modifier.padding(bottom = 16.dp)
-        )
+
+        Spacer(modifier = Modifier.height(24.dp))
+
+        // Name TextField
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                imageVector = Icons.Filled.AccountBox,
+                contentDescription = "Icon Profile",
+                tint = BlueGrey40,
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            OutlinedTextField(
+                value = inputName,
+                onValueChange = { inputName = it },
+                label = { Text("Enter your name") },
+                modifier = Modifier.weight(1f).fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // NIM TextField
+        Row (
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth()
+        ) {
+            Icon(
+                painter = painterResource(id = R.drawable.pin),
+                contentDescription = "Icon Pin",
+                tint = BlueGrey40,
+                modifier = Modifier.size(30.dp)
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+            OutlinedTextField(
+                value = inputNim,
+                onValueChange = { inputNim = it },
+                label = { Text("Enter your NIM") },
+                keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number),
+                modifier = Modifier.fillMaxWidth(),
+                shape = RoundedCornerShape(8.dp)
+            )
+        }
+
+        Spacer(modifier = Modifier.height(16.dp))
+
         Button(
-            onClick = { text = inputText },
+            onClick = {
+                name = inputName
+                nim = inputNim
+            },
             modifier = Modifier.padding(vertical = 4.dp)
         ) {
             Text("Submit")
